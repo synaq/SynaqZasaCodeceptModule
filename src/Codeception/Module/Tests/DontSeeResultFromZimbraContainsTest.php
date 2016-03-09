b4e0f8f4b4e088845aa3240e3bb0212b5157d0f3
willemv <willemv@synaq.com>
2016-03-09 14:26:43 +0200

    Added failure condition test to DontSeeResultFromZimbraContainsTest

--- src/Codeception/Module/Tests/DontSeeResultFromZimbraContainsTest.php
+++ src/Codeception/Module/Tests/DontSeeResultFromZimbraContainsTest.php
@@ -11,18 +11,46 @@ namespace Codeception\Module\Tests;
 
 class DontSeeResultFromZimbraContainsTest extends ZasaModuleTestCase
 {
-    public function testDontSeeResultFromZimbraContains()
+    /**
+     * @test
+     */
+    public function shouldPassIfResultDoesNotContainSubset()
     {
-        $dummyResult = array(
+        $dummyResult = [
             'foo' => 'bar',
-            'baz' => array(
+            'baz' => [
                 'fruit' => 'apple',
                 'veg' => 'tomato',
                 'meat' => 'beef'
-            )
-        );
+            ]
+        ];
+
+        $subset = ['fruit' => 'banana'];
 
         $this->module->_setResult($dummyResult);
-        $this->module->dontSeeResultFromZimbraContains(array('fruit' => 'banana'));
+        $this->module->dontSeeResultFromZimbraContains($subset);
+    }
+
+    /**
+     * @test
+     *
+     * @expectedException \PHPUnit_Framework_ExpectationFailedException
+     * @expectedExceptionMessage I see that {"foo":"bar","baz":{"fruit":"apple","veg":"tomato","meat":"beef"}} contains {"fruit":"apple"}
+     */
+    public function shouldFailIfResultContainsSubset()
+    {
+        $dummyResult = [
+            'foo' => 'bar',
+            'baz' => [
+                'fruit' => 'apple',
+                'veg' => 'tomato',
+                'meat' => 'beef'
+            ]
+        ];
+
+        $subset = ['fruit' => 'apple'];
+
+        $this->module->_setResult($dummyResult);
+        $this->module->dontSeeResultFromZimbraContains($subset);
     }
 }
