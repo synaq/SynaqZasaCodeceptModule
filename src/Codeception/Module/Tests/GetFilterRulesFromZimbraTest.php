53af3129fb06d8753c391d7733898fdc2192c519
Willem Viljoen <willemv@synaq.com>
2021-04-19 15:56:23 +0200

    GetFilterRulesFromZimbra places the returned filter rules in the result from zimbra

--- src/Codeception/Module/Tests/GetFilterRulesFromZimbraTest.php
+++ src/Codeception/Module/Tests/GetFilterRulesFromZimbraTest.php
@@ -35,4 +35,39 @@ class GetFilterRulesFromZimbraTest extends ZasaModuleTestCase
         $this->module->getFilterRulesFromZimbra('bar@baz.com');
         $this->zasa->shouldHaveReceived('getFilterRules')->with('bar@baz.com');
     }
+
+    /**
+     * @test
+     * @throws SoapFaultException
+     */
+    public function placesTheReturnedFilterRulesInTheResultFromZimbra()
+    {
+        $returnedRules = [
+            [
+                'name' => 'Archive_Read',
+                'active' => true,
+                'test_condition' => 'any',
+                'tests' =>
+                    [
+                        [
+                            'test' => 'header',
+                            'stringComparison' => 'matches',
+                            'header' => 'from',
+                            'index' => '0',
+                            'value' => '*'
+                        ]
+                    ],
+                'actions' => [
+                    [
+                        'action' => 'flag',
+                        'flagName' => 'read',
+                        'index' => '0'
+                    ]
+                ]
+            ]
+        ];
+        $this->zasa->shouldReceive('getFilterRules')->andReturn($returnedRules);
+        $this->module->getFilterRulesFromZimbra('bar@baz.com');
+        $this->assertEquals($returnedRules, $this->module->grabResultFromZimbra());
+    }
 }
